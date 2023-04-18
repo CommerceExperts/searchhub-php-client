ef9d56e1e0ed8118b243d7e8693f1efe746fd93c
Siegfried Schüle <siegfried.schuele@commerce-experts.com>
2023-04-18 15:15:44 +0200

    make sure user query is lowercase

--- src/SearchHub/Client/SearchHubRequest.php
+++ src/SearchHub/Client/SearchHubRequest.php
@@ -28,7 +28,7 @@ class SearchHubRequest
 
     public function __construct(string $userQuery)
     {
-        $this->userQuery = $userQuery;
+        $this->userQuery = strtolower($userQuery);
     }
 
     /**
@@ -38,7 +38,7 @@ class SearchHubRequest
      */
     public function setUserQuery($userQuery)
     {
-        $this->userQuery = $userQuery;
+        $this->userQuery = strtolower($userQuery);
 
         return $this;
     }
